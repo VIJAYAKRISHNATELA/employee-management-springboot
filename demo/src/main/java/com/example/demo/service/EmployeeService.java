3660add34037ff00f65940aa820a05bafa49eb95
vijay <vijayakrishnatela@gmail.com>
2026-03-14 10:47:55 +0530

    Implemented Logging(Loggers)

--- demo/src/main/java/com/example/demo/service/EmployeeService.java
+++ demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,18 +1,22 @@
 package com.example.demo.service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.entity.Employee;
 import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    //Logger
+private static final Logger log=LoggerFactory.getLogger(EmployeeService.class);
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -29,13 +33,20 @@ public class EmployeeService {
 
     // Get single employee by ID
     public Employee getEmployeeById(Long id) {
+        log.info("Fetching employee with id {}", id);
+
         return employeeRepository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+                .orElseThrow(() -> {
+                    log.error("Employee not found with id {}", id);
+                    return new EmployeeNotFoundException("Employee not found with id: " + id);
+                });
     }
 
     // Delete employee by ID
     public void deleteEmployee(Long id) {
+        log.info("Deleting employee with id {}",id);
         employeeRepository.deleteById(id);
+        log.info("Employee deleted successfullly eith id{}",id);
     }
 
     // Update employee
@@ -57,6 +68,7 @@ public class EmployeeService {
 
     // Find employee by email
     public Employee getEmployeeByEmail(String email) {
+
         return employeeRepository.findByEmail(email);
     }
 
@@ -66,16 +78,24 @@ public class EmployeeService {
     }
 
     public Employee saveEmployee(EmployeeDto dto){
-        Employee employee=new Employee();
+
+        log.info("Creating new employee with email: {}", dto.getEmail());
+
+        Employee employee = new Employee();
         employee.setName(dto.getName());
         employee.setDepartment(dto.getDepartment());
         employee.setEmail(dto.getEmail());
 
-        return employeeRepository.save(employee);
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        log.info("Employee created successfully with id: {}", savedEmployee.getId());
+
+        return savedEmployee;
     }
 
     //Pagination
    public Page<Employee> getEmployees(Pageable pageable){
         return employeeRepository.findAll(pageable);
    }
+
 }
